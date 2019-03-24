9edb61f23c2a913de2ab0edde3d3002ff9b8a7ce
Tommie <tommiealcorn@gmail.com>
2019-03-24 17:55:31 -0600

    Strings & Booleans

---
+++ js/app.js
@@ -0,0 +1,40 @@
+/*
+
+A.  Q + A
+
+1.  Let or const = the value
+2.  variable = new value
+3.  let New variable = old variable
+4.  Declare is when you initially set up a variable
+    Assign is when you give the variable a value
+    Define I believe is synonymous with Declare
+5.  I, myself, spent about 60/40
+
+B. Strings
+
+1.  let firstVariable = "Hello World"
+        firstVariable = 5
+    let secondVariable = firstVariable
+        secondVariable = "any string"
+        5
+2.  let yourName = "Tommie"
+    console.log('Hello, my name is ' + yourName)
+
+C.  Booleans
+
+    <
+    > 
+    ===
+    !=
+    == == == == == !=
+    ==
+    ===
+    < >
+    === !=
+    ==
+
+D. The Farm
+
+
+
+*/

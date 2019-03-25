6a3e7a3437b6a9037650cffc910a26915bf8d427
Tommie <tommiealcorn@gmail.com>
2019-03-24 19:28:44 -0600

    Multiples of 3 and 5

--- js/app.js
+++ js/app.js
@@ -91,4 +91,16 @@ D.  Savings account
         (bank_account += i) * 2;
     }
 
+
+E.  Multiples of 3 and 5
+
+    let total = 0;
+    for(let i = 1; i < 1000; i++) {
+        if(i % 3 === 0 || i % 5 === 0) {
+            total += i;
+        }
+        console.log(total);
+    }
+    
+
 */
